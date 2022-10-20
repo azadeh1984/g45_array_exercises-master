088c854459349dd534d4ca67449e8069f5505b94
azadeh1984 <azadeh_n1984@yahoo.com>
2022-10-20 15:20:18 +0200

    ex3

--- src/main/java/se/lexicon/Exercise04.java
+++ src/main/java/se/lexicon/Exercise04.java
@@ -5,11 +5,26 @@ public class Exercise04 {
   /**
    * 4. Write a program which will copy the elements of one array into another array.
    * Expected output:
-   *                Elements from first array: 1 15 20
-   *                Elements from second array: 1 15 20
+   * Elements from first array: 1 15 20
+   * Elements from second array: 1 15 20
    */
-  public static void ex4() {
+  public static void ex4(){
+    int[] arr1 = new int[]{1, 15, 20};
+    int arr2[] = new int[arr1.length];
+    for (int i = 0; i < arr1.length; i++) {
+      arr2[i] = arr1[i];
+    }
+    System.out.println("Elements of original array: ");
+    for (int i = 0; i < arr1.length; i++) {
+      System.out.print(arr1[i] + " ");
+    }
+    System.out.println();
 
+    System.out.println("Elements of new array: ");
+    for (int i = 0; i < arr2.length; i++) {
+      System.out.print(arr2[i] + " ");
+    }
   }
-
 }
+
+
